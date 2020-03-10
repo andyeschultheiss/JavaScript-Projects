47bb703d2a4f61fd24655c9c5e5fc55530604fbd
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-03-10 12:18:40 -0700

    First JS examples

---
+++ JS/Basic_JS_Alert.js
@@ -0,0 +1,2 @@
+
+window.alert("Danger! High Voltage!");
---
+++ JS/Basic_JS_strings.js
@@ -0,0 +1,21 @@
+
+var A = "This is a string.";
+window.alert(A);
+
+var B = "This is also a string. ";
+document.write(B);
+
+var C = "\"Keep your friends close, and your enemies closer,\" he whispered. ";
+document.write(C);
+
+var D = "Old McDonald had a farm, " + "E-I-E-I-O! ";
+document.write(D);
+
+var E = "I've got the ";
+var F = "blues. ";
+var F = F.fontcolor("blue");
+document.write(E + F);
+
+var G = 42;
+
+document.write("The answer to life. the universe, and everything is: " + G);
